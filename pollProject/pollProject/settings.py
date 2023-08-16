05401d3db30d3ee1501622088e2b4522a626d305
Bek Brace <60483846+BekBrace@users.noreply.github.com>
2023-08-16 11:12:47 -0700

    Update settings.py
    deleteing Secret_Key

--- pollProject/pollProject/settings.py
+++ pollProject/pollProject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3^-r#s^buula9t1^y3)%#fty76o9%7+htx8g_vt^4kcofgyd$@'
+SECRET_KEY = YOUR OWN KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
